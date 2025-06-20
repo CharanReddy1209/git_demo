fbd5c034c8e27de11e4b848b582a857a27fe3493
charan <gajulapallicharan@gmail.com>
2025-06-20 11:23:54 +0530

    comment in the python.py file in main

--- python.py
+++ python.py
@@ -1,1 +1,3 @@
 print(f"Hello World!")
+# Hello World! is a simple program that prints "Hello World!" to the console.
+# It is often used as a beginner's first program in many programming languages.
